019ef0d73d2248f5cfc4317b5270eb355f671ded
fefifofum <jorsellis@gmail.com>
2016-12-29 15:48:12 +0100

    v500: Rebrand to LineageOS
    Change-Id: Ic5d2fbd9a46293efd619d7d25a36b4a7eb93d54b

--- lineage.mk
+++ lineage.mk
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/lge/v500/device.mk)
 $(call inherit-product-if-exists, vendor/lge/v500/v500-vendor.mk)
 
-# Inherit some common CM stuff
+# Inherit some common LineageOS stuff
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := v500
-PRODUCT_NAME := cm_v500
+PRODUCT_NAME := lineage_v500
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-V500
 PRODUCT_MANUFACTURER := LGE
