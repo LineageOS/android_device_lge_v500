7f745d37666f8b9b0ba5de7ec6cc6bd2125fb09a
Dan Pasanen <dan.pasanen@gmail.com>
2017-03-04 08:35:23 -0600

    v500: inherit gpad83-common
    Change-Id: I7bb9e35c9417e9b1a00a8d9d473d8d4c87d5df2a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,117 +14,25 @@
 # limitations under the License.
 #
 
-# Architecture
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := krait
+# Inherit from common
+-include device/lge/gpad83-common/BoardConfigCommon.mk
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := awifi,v500
 
-# Audio
-BOARD_USES_ALSA_AUDIO:= true
-
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := AWIFI
 TARGET_BOOTLOADER_NAME := awifi
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-BLUETOOTH_HCI_USE_MCT := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/v500/bluetooth
-
-# Board
-TARGET_BOARD_PLATFORM := msm8960
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno320
-
-# Camera
-BOARD_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
-TARGET_HAS_LEGACY_CAMERA_HAL1 := true
-
-# Charger
-BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BOARD_CHARGING_CMDLINE_NAME := "androidboot.mode"
-BOARD_CHARGING_CMDLINE_VALUE := "chargerlogo"
-
-# Dex-preoptimization to speed up first boot sequence
-ifeq ($(HOST_OS),linux)
-  ifeq ($(WITH_DEXPREOPT),)
-    WITH_DEXPREOPT := true
-  endif
-endif
-
-# GPS
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
-BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
-TARGET_NO_RPC := true
-
-# Graphics
-USE_OPENGL_RENDERER := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-TARGET_USES_ION := true
-TARGET_USES_C2D_COMPOSITION := true
-TARGET_DISPLAY_USE_RETIRE_FENCE := true
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-
-# Hardware tunables
-BOARD_HARDWARE_CLASS := device/lge/v500/cmhw/
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/knock_on"
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/lge/v500
 TARGET_KERNEL_CONFIG := lineageos_v500_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 ehci-hcd.park=3 lpj=67677 androidboot.hardware=awifi vmalloc=400M
-BOARD_KERNEL_BASE := 0x80200000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
-BOARD_CUSTOM_BOOTIMG_MK := device/lge/v500/mkbootimg.mk
-BOARD_CUSTOM_BOOTIMG := true
-
-# Legacy stuff
-BOARD_GLOBAL_CFLAGS += -DQCOM_BSP_LEGACY
-TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 
 # Partitions
-TARGET_USERIMAGES_USE_EXT4         := true
-TARGET_USERIMAGES_USE_F2FS         := true
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE  := ext4
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 25165824    # 24M
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 838860800   # 800M
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 25165824    # 24M
 BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 2248146944  # 2144M
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12071206912 # 11512M
-BOARD_CACHEIMAGE_PARTITION_SIZE    := 838860800   # 800M
-BOARD_FLASH_BLOCK_SIZE             := 131072      # (BOARD_KERNEL_PAGESIZE * 64)
-
-# Recovery
-TARGET_RECOVERY_FSTAB := device/lge/v500/rootdir/fstab.awifi
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-
-# SELinux policies
-include device/qcom/sepolicy/sepolicy.mk
-
-BOARD_SEPOLICY_DIRS += device/lge/v500/sepolicy
-
-# Snapdragon LLVM
-TARGET_USE_SDCLANG := true
-
-# Wifi
-BOARD_HAS_QCOM_WLAN := true
-BOARD_WLAN_DEVICE := qcwcn
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_DRIVER_FW_PATH_STA := "sta"
-WIFI_DRIVER_FW_PATH_AP  := "ap"
 
 # inherit from the proprietary version
 -include vendor/lge/v500/BoardConfigVendor.mk
