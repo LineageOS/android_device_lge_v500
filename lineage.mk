b12d787bc402b6e53615fb1993597fafcdaf72d6
Dan Pasanen <dan.pasanen@gmail.com>
2017-08-28 13:58:20 -0500

    v500: inherit vendor/lineage
    Change-Id: Idb98cef70480de993d21f6d7cfaf99a12a62ff5d

--- lineage.mk
+++ lineage.mk
@@ -22,7 +22,7 @@ $(call inherit-product, device/lge/v500/device.mk)
 $(call inherit-product-if-exists, vendor/lge/v500/v500-vendor.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := v500
